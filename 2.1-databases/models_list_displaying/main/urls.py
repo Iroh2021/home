f188d4198de6243fcc9ce87e7a744ca6ec27ef5d
Iroh_2105 <ershovoleg18@mail.ru>
2023-09-17 13:40:59 +0300

    homework 2.1

--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, register_converter
 
-from books.views import books_view
+from books.views import books_view, date_view
+from books import converters
+
+register_converter(converters.DateConverter, 'date')
+
 
 urlpatterns = [
     path('', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('books/', books_view, name='catalog'),
+    path('books/<date:pub_date>/', date_view, name='catalog_date'),
 ]
