c65c30d389067d3d59f00a22f6ea5b7cc9fbf878
Iroh_2105 <ershovoleg18@mail.ru>
2023-10-18 15:00:12 +0300

    homework 3.1

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Датчик')
+    description = models.CharField(max_length=100)
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'Датчик'
+        verbose_name_plural = 'Датчики'
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    temperature = models.FloatField(verbose_name='Температура')
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Дата измерения')
+    photo = models.ImageField(null=True, verbose_name='Изображение')
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name="measurements")
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'Измерение'
+        verbose_name_plural = 'Измерения'
--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from django.contrib import admin
+
+from measurement.views import SensorListCreate, MeasurementCreate, SensorRetrieveUpdate
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('admin/', admin.site.urls),
+    path('sensors/', SensorListCreate.as_view()),
+    path('measurements/', MeasurementCreate.as_view()),
+    path('sensors/<pk>/', SensorRetrieveUpdate.as_view()),
 ]
