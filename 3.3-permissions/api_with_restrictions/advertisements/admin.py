01a98e455e9f2500ea6c51e22e7b507342be4ba0
Iroh_2105 <ershovoleg18@mail.ru>
2024-04-02 12:49:55 +0300

    home

--- 3.3-permissions/api_with_restrictions/advertisements/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from advertisements.models import *
-
-
-@admin.register(Advertisement)
-class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'creator', 'title', 'description', 'status']
--- 3.3-permissions/api_with_restrictions/advertisements/permission.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-
-class IsAdminPermission(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser:
-            return True
-        return obj.creator == request.user
